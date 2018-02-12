9c9df736e0d8804fa8a7d9138329e4ae6aa2294c
罗晓俊 <gogitservice@gmail.com>
2018-02-12 23:20:21 +0800

    增加最大和最小请求耗时

--- start.php
+++ start.php
@@ -22,19 +22,35 @@ $ssl = boolval($argv[5] ?? 1);
 $executeTime = new chan($c);
 
 go(function () use ($executeTime, $c){
+    $minTime = 0;
+    $maxTime = 0;
     $totalTime = 0;
     $executedTimes = 0;
     while($executedTimes < $c) {
-    	if($time = $executeTime->pop()) {
-	    $totalTime += $time;
+        if ($time = $executeTime->pop()) {
+            $totalTime += $time;
+            if ($minTime <= 0 || $minTime > $time) {
+                $minTime = $time;
+            }
+            if ($time > $maxTime) {
+                $maxTime = $time;
+            }
             ++$executedTimes;
-	}
+        }
     }
     echo '请求并发: ';
     echo $executedTimes;
     echo PHP_EOL;
     echo '平均耗时: ';
     echo ($totalTime / $c) * 1000;
+    echo '毫秒';
+    echo PHP_EOL;
+    echo '最大耗时: ';
+    echo $maxTime * 1000;
+    echo '毫秒';
+    echo PHP_EOL;
+    echo '最小耗时: ';
+    echo $minTime * 1000;
     echo '毫秒';
     echo PHP_EOL;
 });
