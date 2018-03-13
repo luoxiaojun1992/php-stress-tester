f136faa023591882380bfb1f87508913611af9d3
罗晓俊 <gogitservice@gmail.com>
2018-03-13 22:08:15 +0800

    bugfix

--- tools/compress.php
+++ tools/compress.php
@@ -4,5 +4,7 @@ $phar = new Phar('pst.phar', 0, 'pst.phar');
 $phar->buildFromDirectory(__DIR__ . '/../src');
 $phar->setStub(Phar::createDefaultStub('start.php', 'start.php'));
 $phar->compressFiles(Phar::GZ);
-mkdir(__DIR__ . '/../bin', 0777, true);
+if (!is_dir(__DIR__ . '/../bin')) {
+    mkdir(__DIR__ . '/../bin', 0777, true);
+}
 shell_exec('mv ' . __DIR__ . '/pst.phar ' . __DIR__ . '/../bin/pst.phar');
