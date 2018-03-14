cc1b49458d349feb849fd6a78cee8eede8c01714
罗晓俊 <gogitservice@gmail.com>
2018-03-14 09:52:54 +0800

    add shell

--- tools/compress.php
+++ tools/compress.php
@@ -3,7 +3,7 @@
 
 $phar = new Phar('pst.phar', 0, 'pst.phar');
 $phar->buildFromDirectory(__DIR__ . '/../src');
-$phar->setStub(Phar::createDefaultStub('start.php', 'start.php'));
+$phar->setStub('#! /usr/bin/env php' . PHP_EOL . Phar::createDefaultStub('start.php', 'start.php'));
 $phar->compressFiles(Phar::GZ);
 if (!is_dir(__DIR__ . '/../bin')) {
     mkdir(__DIR__ . '/../bin', 0777, true);
